58ca92971d76a9ede00fffb07730a476ed09f38c
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-02-09 16:32:23 -0700

    Tasks API Pagination and Custom Permissions

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from .views import CategoryViewSet
+from .views import CategoryViewSet, TaskViewSet
 
 routers = routers.DefaultRouter()
 routers.register(r'api/categories', CategoryViewSet, 'categories')
+routers.register(r'api/tasks', TaskViewSet, 'tasks')
 
 urlpatterns = routers.urls
